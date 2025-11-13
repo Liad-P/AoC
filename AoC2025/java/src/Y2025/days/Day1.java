47a4ceea01dc846d48917e160140e9085149a258
Liad Peretz <liad@synthesis.co.za>
2025-11-13 18:04:14 +0200

    Initial Commit for AoC2025

---
+++ AoC2025/java/src/Y2025/days/Day1.java
@@ -0,0 +1,13 @@
+package Y2025.days;
+
+import Y2025.utils.Utils;
+
+public class Day1 implements IDay{
+
+    @Override
+    public void solve(){
+        System.out.println("Hello from day 1");
+        Utils.getInputFileOfDay(2025,1);
+    }
+
+}
