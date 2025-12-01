63aa67bac42b27f4d24517556ec8f87717675ecf
Liad Peretz <peretz.liad@gmail.com>
2025-12-01 16:53:29 +0200

    AoC2025 Day 1, refactored, concise

--- AoC2025/haskell/src/AoC2025Day1.hs
+++ AoC2025/haskell/src/AoC2025Day1.hs
@@ -5,27 +5,22 @@ module AoC2025Day1 where
 import Text.Parsec
 import Text.Parsec.String (Parser)
 import qualified Debug.Trace as Debug
+import Data.Functor
 
-lineParser:: Parser Int
-lineParser = do
-    op <- operator
-    integer <- read <$> many1 digit
-    pure (op * integer)
-    where
-        operator:: Parsec String st Int
-        operator =
-            try (char 'L') *> pure (-1)
-            <|> (char 'R') *> pure 1
+lineParser:: Parser [Int]
+lineParser =
+    many1 $
+        (*) <$> ((char 'L' $> (-1)) <|> (char 'R' $> 1))
+            <*> (read <$> many1 digit <* optional endOfLine)
 
-parseFile:: [String] -> Parser a -> [a]
+
+
+parseFile:: String -> Parser a -> a
 parseFile lines p =
-    foldl (\acc line ->
-            let a = parse p "" line
-            in case a of
-                Left err -> error "Error passing file"
-                Right res -> acc ++ [res])
-                []
-                lines
+    let a = parse p "" lines
+        in case a of
+            Left err -> error "Error passing file"
+            Right res -> res
 
 countZeroSums:: [Int] -> Int
 countZeroSums ns =
@@ -43,36 +38,28 @@ countZeroSums ns =
 
 part1:: IO ()
 part1 = do
-    inputLines <- lines <$> readFile "../input/day1.txt"
+    inputLines <- readFile "../input/day1.txt"
     let turns = parseFile inputLines lineParser
     print $ countZeroSums turns
 
 
-countPassesOfZeros:: [Int] -> Int
-countPassesOfZeros ns =
-    snd $
-    foldl
-        (\acc c ->
-
-            let newSum = (fst acc) + c
-                newAcc = newSum `mod` 100
-                passesOf0 = calculatePassesOfZero (fst acc) c
-            in
-                Debug.trace ("STEP:\nOld acc = " ++ show acc ++ "\nChange val = " ++ show c ++ "\nCalculated Passes Of Zero = " ++ show passesOf0) (newAcc, snd acc + passesOf0)
-        )
-        (50,0)
-        ns
-
 calculatePassesOfZero:: Int -> Int -> Int
-calculatePassesOfZero cur inc = val `div` 100
+calculatePassesOfZero cur inc = (val + abs inc) `div` 100
     where
         val
-          | cur == 0 = abs inc
-          | inc < 0 = 100 - cur + abs inc
-          | otherwise = cur + inc
+        -- if cur == 0, and inc < 0: example inc = -5, then cur + inc = -5, -5/100 = -1 but we didnt pass 0, therefore rather do cur + abs(inc) = 5 in this case
+        -- Other case: example cur = 5, inc =98: then work as expected 103/100 = 1 which is as expected.
+          | inc >= 0 || cur == 0 = cur
+        -- Case where inc is negative and cur != 0: then shift curr into the positive space
+          | otherwise = 100 - cur
 
-part2:: IO ()
+part2:: IO Int
 part2 = do
-    inputLines <- lines <$> readFile "../input/day1.txt"
+    inputLines <- readFile "../input/day1.txt"
     let turns = parseFile inputLines lineParser
-    print $ countPassesOfZeros turns
+    pure $
+        snd $
+            foldl
+                (\(acc, count) c -> ((acc + c) `mod` 100, count + calculatePassesOfZero acc c))
+                (50,0)
+                turns
