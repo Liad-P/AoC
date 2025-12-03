71c4c0b95a08281982129dee23c5a4aec1617660
Liad Peretz <peretz.liad@gmail.com>
2025-12-03 13:22:16 +0200

    AoC2025 day 3 - quick and messy

--- AoC2025/haskell/src/AoC2025Day3.hs
+++ AoC2025/haskell/src/AoC2025Day3.hs
@@ -3,6 +3,8 @@ module AoC2025Day3 where
 import Text.Parsec
 import Text.Parsec.String (Parser)
 import Data.Char (digitToInt)
+import Data.List (find, findIndex, elemIndex)
+import qualified Debug.Trace as Debug
 
 fileToBanks:: String -> IO [[Int]]
 fileToBanks inputFile = do
@@ -40,4 +42,35 @@ part1 = do
     banks <- fileToBanks "../input/day3.txt"
     let maxDigits = map get2MaxDigits banks
         totalJoltage = sum $ map (\(a,b) -> a*10 + b) maxDigits
+    print totalJoltage
+
+intArrayToInt:: [Int] -> Int
+intArrayToInt xs = go 0 xs
+    where
+        go acc arr
+            | null arr = acc
+            | otherwise = go (acc + (head arr * 10 ^ (length arr - 1))) (tail arr)
+
+getNMaxDigits:: Int -> [Int] -> Int
+getNMaxDigits n inputDigits = intArrayToInt possiblePairs
+    where
+        possiblePairs::[Int]
+        possiblePairs = go inputDigits []
+        go nums acc 
+            | length acc == n = acc
+            | (length nums + length acc) == n = acc ++ nums
+            | otherwise = go (drop (indexOfNextMax+1) nums) (acc ++ [nextMax])
+            where
+                numsToGetMaxFrom = take (length nums - (n - length acc) + 1) nums
+                nextMax = maximum numsToGetMaxFrom
+                indexOfNextMax = case elemIndex nextMax numsToGetMaxFrom of 
+                    Just m -> m
+                    Nothing -> error ("The max was not found" ++ show nextMax)
+
+
+part2:: IO ()
+part2 = do
+    banks <- fileToBanks "../input/day3.txt"
+    let maxDigits = map (getNMaxDigits 12) banks
+        totalJoltage = sum maxDigits
     print totalJoltage
