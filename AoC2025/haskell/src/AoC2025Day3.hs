24652d442d5d7cad9bbe30fbe877cade3f4bacfa
Liad Peretz <peretz.liad@gmail.com>
2025-12-03 13:44:40 +0200

    AoC2025 day 3 part 2 refactor and comments

--- AoC2025/haskell/src/AoC2025Day3.hs
+++ AoC2025/haskell/src/AoC2025Day3.hs
@@ -9,14 +9,8 @@ import qualified Debug.Trace as Debug
 fileToBanks:: String -> IO [[Int]]
 fileToBanks inputFile = do
     lines <- lines <$> readFile inputFile
-    let banks= map (\s -> map digitToInt s) lines
+    let banks= map (map digitToInt) lines
     pure banks
-
--- getNMaxDigits:: Int -> [Int] -> [Int]
--- getNMaxDigits n inputDigits = 
---     foldl (\acc elem -> 
-
---         )
 
 get2MaxDigits:: [Int] -> (Int, Int)
 get2MaxDigits inputDigits =
@@ -34,7 +28,6 @@ get2MaxDigits inputDigits =
             | length nums < 2 = acc
             | length nums == 2 = acc ++ [(head nums, last nums)]
             | otherwise = go (tail nums) (acc ++ [(head nums, maximum $ tail nums)])
-        
 
 
 part1:: IO ()
@@ -45,24 +38,30 @@ part1 = do
     print totalJoltage
 
 intArrayToInt:: [Int] -> Int
-intArrayToInt xs = go 0 xs
+intArrayToInt = go 0
     where
         go acc arr
             | null arr = acc
             | otherwise = go (acc + (head arr * 10 ^ (length arr - 1))) (tail arr)
 
-getNMaxDigits:: Int -> [Int] -> Int
-getNMaxDigits n inputDigits = intArrayToInt possiblePairs
+getNJoltageFromBank:: Int -> [Int] -> Int
+getNJoltageFromBank nBatteries bank = intArrayToInt $ go bank []
     where
-        possiblePairs::[Int]
-        possiblePairs = go inputDigits []
         go nums acc 
-            | length acc == n = acc
-            | (length nums + length acc) == n = acc ++ nums
-            | otherwise = go (drop (indexOfNextMax+1) nums) (acc ++ [nextMax])
+            -- we have got the desired batteries that we want to turn on
+            | length acc == nBatteries = acc
+            -- we the number of batteries we have gotten + the number of batteries left in the bank is equal to the number of batteries we want
+            | (length nums + length acc) == nBatteries = acc ++ nums
+            -- Otherwise, we can continue iterating through the rest of the nums
+            -- only consider betteries after the bettery we just added to our chosen collection
+            | otherwise = go (drop (indexOfNextMax + 1) nums) (acc ++ [nextMax])
             where
-                numsToGetMaxFrom = take (length nums - (n - length acc) + 1) nums
+                -- get the batteries that we should consider for adding to our collection
+                -- we should only consider batteries that could be chosen while still allowing us to get to the n desired batteries in our collection
+                numsToGetMaxFrom = take (length nums - (nBatteries - length acc) + 1) nums
+                -- Choose the bettery with the maximum joltage from the options that make sense
                 nextMax = maximum numsToGetMaxFrom
+                -- get the index of the battery we chose with the maximum joltage
                 indexOfNextMax = case elemIndex nextMax numsToGetMaxFrom of 
                     Just m -> m
                     Nothing -> error ("The max was not found" ++ show nextMax)
@@ -71,6 +70,4 @@ getNMaxDigits n inputDigits = intArrayToInt possiblePairs
 part2:: IO ()
 part2 = do
     banks <- fileToBanks "../input/day3.txt"
-    let maxDigits = map (getNMaxDigits 12) banks
-        totalJoltage = sum maxDigits
-    print totalJoltage
+    print $ sum $ map (getNJoltageFromBank 12) banks
